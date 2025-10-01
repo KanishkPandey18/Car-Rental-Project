ebc972f8d043419c87e067b134f05277846e2dec
KanishkPandey18 <kanishkpandey240@gmail.com>
2025-10-02 01:47:56 +0530

    Home page done

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero'
+import CarCard from '../Components/CarCard'
+import FeaturedSection from '../Components/FeaturedSection'
+// import dummyCarData from '../assets/assets'
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <FeaturedSection/>
         </div>
     )
 }
