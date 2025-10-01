0db524dfbf3dc575432cd8992c032338bc9f10c3
KanishkPandey18 <kanishkpandey240@gmail.com>
2025-10-01 21:02:22 +0530

    Navbar done and pages initialized

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Pages/Home'
+import CarDetails from './Pages/CarDetails'
+import Cars from './Pages/Cars'
+import MyBookings from './Pages/MyBookings'
 
 const App = () => {
+
+  const[showLogin,setShowLogin]=useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith('/owner') ;
   return (
-    <div>
-      
-    </div>
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/mybookings' element={<MyBookings/>}/>
+      </Routes>
+    </>
   )
 }
 
