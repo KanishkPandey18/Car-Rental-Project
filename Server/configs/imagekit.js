0f696e2ed757bba0a85a0850e13dab028277d958
KanishkPandey18 <kanishkpandey240@gmail.com>
2026-01-25 12:58:38 +0530

    done

--- Server/configs/imagekit.js
+++
@@ -1,9 +0,0 @@
-import ImageKit from '@imagekit/nodejs';
-
-const imagekit = new ImageKit({
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,     // This is the default and can be omitted
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,   // This is the default and can be omitted
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT  // This is the default and can be omitted
-});
-
-export default imagekit;    // Exports the client object
